e8f3648805c38def95f068c6f77c117f90169a46
Livingston Pradeep Aloysius John <livingstonpradeepaloysiusjohn@Livingstons-MacBook-Pro.local>
2025-02-26 11:06:18 +0100

    adding Base URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,47 +37,12 @@ export default defineConfig({
     {
       name: 'Realestate_Demo',
       use: { ...devices['Desktop Chrome'],
-        baseURL: "http://localhost:5173/",
-        headless: false,
+        baseURL: process.env.PLAYWRIGHT_BASE_URL || "http://localhost:5173/",
+        headless: true,
         screenshot: 'only-on-failure',
        },
     },
 
-    // {
-    //   name: 'Realestate_Demo',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'Realestate_Demo',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
